c5eb1a49e89377a714840861d3b00b7a8c654ee6
yiye <yiye.js@gmail.com>
2018-04-07 21:42:06 +0800

    first commit

---
+++ man.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the boss!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world');
+})();
